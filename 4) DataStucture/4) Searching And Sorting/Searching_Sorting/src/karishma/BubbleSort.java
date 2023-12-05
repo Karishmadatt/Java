fcb5ce10c657556e426bc17afa466bce75ffe0f0
Karishma <karishmadatt2000@gmail.com>
2023-12-05 12:48:52 +0530

    completed bubble sort

--- 4) DataStucture/4) Searching And Sorting/Searching_Sorting/src/karishma/BubbleSort.java
+++ 4) DataStucture/4) Searching And Sorting/Searching_Sorting/src/karishma/BubbleSort.java
@@ -1,5 +1,34 @@
 package karishma;
 
-public class BubbleSort {
+import java.util.Scanner;
 
+public class BubbleSort {
+	
+	public static void bubblesort(int arr[],int n) {
+		boolean check;int temp;
+		for(int i=0;i<n-1;i++) {
+			check=false;
+			for(int j=0;j<n-i-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					temp= arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+					check=true;
+				}
+			}
+			if(!check) {
+				break;
+			}
+		}
+	}
+	
+public static void main(String[] args) {
+	Utility u = new Utility();
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int arr[]=new int[n];
+	u.arrayin(arr, n);
+	bubblesort(arr,n);
+	u.arrayout(arr, n);
+}
 }
