394eeb4830cf09eabed0eb32d827fe7d3e2b2957
Karishma <karishmadatt2000@gmail.com>
2023-12-05 17:49:17 +0530

    insertion sort

--- 4) DataStucture/4) Searching And Sorting/Searching_Sorting/src/karishma/InsertionSort.java
+++ 4) DataStucture/4) Searching And Sorting/Searching_Sorting/src/karishma/InsertionSort.java
@@ -1,5 +1,27 @@
 package karishma;
 
-public class InsertionSort {
+import java.util.Scanner;
 
+public class InsertionSort {
+	
+	public static void insertionsort(int arr[],int n) {
+		for(int i=1;i<n;i++) {
+			int j=i-1;int key=arr[i];
+			while(j>=0 && arr[j]>key) {
+				arr[j+1]=arr[j];
+				j-=1;
+			}
+			arr[j+1]=key;
+		}
+	}
+	
+public static void main(String[] args) {
+	Utility u = new Utility();
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int arr[]=new int[n];
+	u.arrayin(arr, n);
+	insertionsort(arr,n);
+	u.arrayout(arr, n);
+}
 }
