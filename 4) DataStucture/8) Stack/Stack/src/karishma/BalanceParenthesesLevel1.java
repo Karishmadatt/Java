ea340d1b37e59b722b53a5fe10273e3ed903c2dd
Karishma <karishmadatt2000@gmail.com>
2023-12-06 19:31:20 +0530

    completed balance parentheses

--- 4) DataStucture/8) Stack/Stack/src/karishma/BalanceParenthesesLevel1.java
+++ 4) DataStucture/8) Stack/Stack/src/karishma/BalanceParenthesesLevel1.java
@@ -5,36 +5,46 @@ import java.util.Stack;
 
 public class BalanceParenthesesLevel1 {
 	
-public static void solve(Stack<Character> stk,String s,int i) {
-	boolean check = true;
-	while(check) {
+public static boolean solve(Stack<Character> stk,String s) {
+    int n = s.length()-1;char c;
+	for(int i=0;i<=n;i++) {
 		if(s.charAt(i)=='{' || s.charAt(i)=='(' || s.charAt(i)=='[') {
-			stk.push(s.charAt(i));i++;
+			stk.push(s.charAt(i));
+			continue;
 		}
-		else {
-			check=false;
+		if(stk.isEmpty())
+			return false;
+		   switch(s.charAt(i)) {
+		   case '}':
+			   c=stk.pop();
+			   if(c=='(' || s.charAt(i)=='[')
+				   return false;
+			   break;
+		   case ']':
+			   c=stk.pop();
+			   if(c=='(' || s.charAt(i)=='{')
+				   return false;
+			  break;
+		   case ')':
+			   c=stk.pop();
+			   if(c=='[' || s.charAt(i)=='{')
+				   return false;
+			   break;
+		   }
 		}
-	}
-	System.out.println(i);
-	while(!stk.isEmpty()) {
-		if((s.charAt(i)=='(' && stk.peek()!=')') ||( s.charAt(i)=='{' && stk.peek()!='}')  || (s.charAt(i)=='[' && stk.peek()!=']') ) {
-			break;
-		}else {
-			i++;
-			stk.pop();
-		}
-	}
-	if(stk.isEmpty()) {
-		System.out.println("Balanced");
-	}else {
-		System.out.println("Not Balanced");
-	}
+	
+	return (stk.isEmpty());
+	
 }
 	
 public static void main(String[] args) {
 	Stack<Character> stk = new Stack<>();
 	Scanner sc = new Scanner(System.in);
 	String s = sc.next();
-	solve(stk,s,0);
+	if(solve(stk,s)) {
+		System.out.println("Balanced");
+	}else {
+		System.out.println("Not Balanced");
+	}
 }
 }
